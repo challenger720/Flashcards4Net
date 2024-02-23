c1d71de58cb5bcb12a313580cb3f4c75b094dac1
challenger720 <127072307+challenger720@users.noreply.github.com>
2024-02-22 19:09:37 -0700

    change DHCP - lower

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ Random random = new Random();
 int n;
 string? answer;
 string[,] ports = { { "22", "SSH"}, { "53", "DNS" }, { "22", "SFTP" }, { "20", "FTP (lower)" }, { "21", "FTP (higher)" }, { "69", "TFTP" }, { "23", "TELNET" }
-, { "67", "DHCP (lower)" }, { "68", "DHCP (higher)" }, { "80", "HTTP" }, { "443", "HTTPS" }, { "161", "SNMP (lower)" }, { "162", "SNMP (higher)" }, { "3389", "RDP" }, { "123", "NTP" }
+, { "67", "DHCP - lower" }, { "68", "DHCP (higher)" }, { "80", "HTTP" }, { "443", "HTTPS" }, { "161", "SNMP (lower)" }, { "162", "SNMP (higher)" }, { "3389", "RDP" }, { "123", "NTP" }
 , { "5060", "SIP" }, { "5061", "SIP" }, { "445", "SMB" }, { "25", "SMTP" }, { "587", "SMTP(TLS)" }, { "110", "POP/POP3" }, { "995", "POP3(TLS)" }, { "143", "IMAP" }
 , { "993", "IMAP(SSL)" }, { "389", "LDAP" }, { "636", "LDAPS" }, { "514", "Syslog" }, { "1433", "SQL Server" }, { "1521", "SQLnet" }, { "3306", "MySQL" } };
 
